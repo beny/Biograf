b43b44883cc0c30dfb2bfe2ee4152f54f29879cd
Ondřej Beneš <ondra.benes@gmail.com>
2012-08-23 07:02:27 +0200

    Init commit

---
+++ Biograf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Biograf
+//
+//  Created by Ondra Beneš on 8/23/12.
+//  Copyright (c) 2012 Ondra Beneš. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
